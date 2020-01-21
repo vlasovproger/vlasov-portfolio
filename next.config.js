c221fef98f0e4132e969781f5e26e6f77e240a4c
Andrei <vlasovproger@gmail.com>
2020-01-21 20:46:04 +0300

    Added Preact optimization

--- next.config.js
+++ next.config.js
@@ -2,33 +2,44 @@ const withSass = require("@zeit/next-sass");
 const withOffline = require("next-offline");
 
 const nextConfig = {
-  target: 'serverless',
-  transformManifest: manifest => ['/'].concat(manifest), // add the homepage to the cache
+  webpack: function(config) {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      react: "preact/compat",
+      react$: "preact/compat",
+      "react-dom": "preact/compat",
+      "react-dom$": "preact/compat"
+    };
+
+    return config;
+  },
+  target: "serverless",
+  transformManifest: manifest => ["/"].concat(manifest), // add the homepage to the cache
   // Trying to set NODE_ENV=production when running yarn dev causes a build-time error so we
   // turn on the SW in dev mode so that we can actually test it
   generateInDevMode: false,
   dontAutoRegisterSw: true,
   workboxOpts: {
-    swDest: 'static/service-worker.js',
+    swDest: "static/service-worker.js",
     maximumFileSizeToCacheInBytes: 5000000,
     runtimeCaching: [
       {
         urlPattern: /^https?.*/,
-        handler: 'NetworkFirst',
+        handler: "NetworkFirst",
         options: {
-          cacheName: 'https-calls',
+          cacheName: "https-calls",
           networkTimeoutSeconds: 15,
           expiration: {
             maxEntries: 150,
-            maxAgeSeconds: 30 * 24 * 60 * 60, // 1 month
+            maxAgeSeconds: 30 * 24 * 60 * 60 // 1 month
           },
           cacheableResponse: {
-            statuses: [0, 200],
-          },
-        },
-      },
-    ],
-  },
+            statuses: [0, 200]
+          }
+        }
+      }
+    ]
+  }
 };
 
 module.exports = withOffline(withSass(nextConfig));
